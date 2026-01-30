270a1805202c4fcefef26cb4d1ab67f3c879de74
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-01-30 14:27:24 +0800

    'setting up mangodb atlas account'

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
-require('dotenv').config();
-
+require("dotenv").config();
+require("mongoose").connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
