a7a174a38197acaabed1d6a71bc7d9d7a00a36c1
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-01-31 15:17:45 +0800

    3. createAndSavePerson

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
-require("mongoose").connect(process.env.MONGO_URI);
+require("mongoose").connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person = require("./models/person");
 
