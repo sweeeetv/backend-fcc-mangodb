315109faf09254b24e83c7d14b229c23d70a6746
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-02-01 15:50:12 +0800

    7. search db with Model.findId() for _id

--- myApp.js
+++ myApp.js
@@ -53,7 +53,13 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) {
+      done(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findEditThenSave = (personId, done) => {
