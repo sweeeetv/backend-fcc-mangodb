ffd069c6401b82fa8e674095873ab0efd5af9370
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-02-06 08:36:15 +0800

    8. findEditThenSave, old way of edit a document

--- myApp.js
+++ myApp.js
@@ -64,8 +64,18 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) {
+      return done(err);
+    }
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, updatedPerson) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
