a2670a43d7d2b4809bdcc90a36db12bfffb4e197
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-02-06 11:35:56 +0800

    12.

--- myApp.js
+++ myApp.js
@@ -114,8 +114,16 @@ const removeManyPeople = async (nameToRemove, done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
