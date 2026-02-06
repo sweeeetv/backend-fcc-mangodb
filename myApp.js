ada548ec43ace5b57d1e785649ca7b28f8cb6c85
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-02-06 09:23:44 +0800

    9.

--- myApp.js
+++ myApp.js
@@ -80,8 +80,17 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, data) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, data);
+    },
+  );
 };
 
 const removeById = (personId, done) => {
